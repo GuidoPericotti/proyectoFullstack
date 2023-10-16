4f0ab3a9fe30432d8b9eeb249e0663c6f2a77035
GuidoPericotti <guido.pericotti@gmail.com>
2023-10-16 11:57:57 -0300

    actualizacion de api

--- js/api.js
+++ js/api.js
@@ -8,15 +8,15 @@ fetch("/js/comentarios.json")
 
         const nuevaCalificacion2 = data.comentarios[1].calificacion;
         const calificacion2 = document.getElementById("calificacion2");
-        calificacion2.innerText = nuevaCalificacion2;
+        calificacion2.innerText = `Calificación: ${nuevaCalificacion2}`;
 
         const nuevaCalificacion3 = data.comentarios[2].calificacion;
         const calificacion3 = document.getElementById("calificacion3");
-        calificacion3.innerText = nuevaCalificacion3;
+        calificacion3.innerText = `Calificación: ${nuevaCalificacion3}`;
 
         const nuevaCalificacion4 = data.comentarios[3].calificacion;
         const calificacion4 = document.getElementById("calificacion4");
-        calificacion4.innerText = nuevaCalificacion4;
+        calificacion4.innerText = `Calificación: ${nuevaCalificacion4}`;
     })
 //Solicitud de comentarios
 fetch("/js/comentarios.json")
@@ -39,7 +39,8 @@ fetch("/js/comentarios.json")
         comentario4.innerText = nuevoComentario4;
 })
 
-const generarUsuario = async () => {
+//Solicitud de imagen y nombre
+const generarUsuario1 = async () => {
     const url = "https://randomuser.me/api/";
     const respuesta = await fetch(url);
     const {results} = await respuesta.json();
@@ -54,4 +55,52 @@ const generarUsuario = async () => {
     nombre1.innerText = nuevoNombre1;
 }
 
-generarUsuario();
+const generarUsuario2 = async () => {
+    const url = "https://randomuser.me/api/";
+    const respuesta = await fetch(url);
+    const {results} = await respuesta.json();
+    const datos = results[0];
+
+    const nuevaImagen2 = datos.picture.thumbnail;
+    const imagen2 = document.getElementById("imagen2");
+    imagen2.setAttribute("src", nuevaImagen2);
+
+    const nuevoNombre2 = datos.name.first +" "+ datos.name.last;
+    const nombre2 = document.getElementById("nombre2");
+    nombre2.innerText = nuevoNombre2;
+}
+
+const generarUsuario3 = async () => {
+    const url = "https://randomuser.me/api/";
+    const respuesta = await fetch(url);
+    const {results} = await respuesta.json();
+    const datos = results[0];
+
+    const nuevaImagen3 = datos.picture.thumbnail;
+    const imagen3 = document.getElementById("imagen3");
+    imagen3.setAttribute("src", nuevaImagen3);
+
+    const nuevoNombre3 = datos.name.first +" "+ datos.name.last;
+    const nombre3 = document.getElementById("nombre3");
+    nombre3.innerText = nuevoNombre3;
+}
+
+const generarUsuario4 = async () => {
+    const url = "https://randomuser.me/api/";
+    const respuesta = await fetch(url);
+    const {results} = await respuesta.json();
+    const datos = results[0];
+
+    const nuevaImagen4 = datos.picture.thumbnail;
+    const imagen4 = document.getElementById("imagen4");
+    imagen4.setAttribute("src", nuevaImagen4);
+
+    const nuevoNombre4 = datos.name.first +" "+ datos.name.last;
+    const nombre4 = document.getElementById("nombre4");
+    nombre4.innerText = nuevoNombre4;
+}
+
+generarUsuario1();
+generarUsuario2();
+generarUsuario3();
+generarUsuario4();
